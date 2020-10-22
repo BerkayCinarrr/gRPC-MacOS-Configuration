0787d56793ae5fe0414a0a983ae0a7bf9a6e37f6
Berkay Çınar <berkayultra1@hotmail.com>
2020-10-22 19:25:23 +0300

    macOS configuration

--- GrpcGreeter/Program.cs
+++ GrpcGreeter/Program.cs
@@ -1,10 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+using System.Runtime.InteropServices;
 
 namespace GrpcGreeter
 {
@@ -15,12 +12,19 @@ namespace GrpcGreeter
             CreateHostBuilder(args).Build().Run();
         }
 
-        // Additional configuration is required to successfully run gRPC on macOS.
-        // For instructions on how to configure Kestrel and gRPC clients on macOS, visit https://go.microsoft.com/fwlink/?linkid=2099682
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
-                {
+                { 
+                    // Configuration for macOS
+                    // HTTP/2 without TLS should only be used during app development. Production apps should always use transport security. Visit https://go.microsoft.com/fwlink/?linkid=2099682
+                    if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) 
+                     webBuilder.ConfigureKestrel(options =>
+                     {
+                         // Setup a HTTP/2 endpoint without TLS.
+                         options.ListenLocalhost(5000, o => o.Protocols = 
+                             HttpProtocols.Http2);
+                     });
                     webBuilder.UseStartup<Startup>();
                 });
     }
